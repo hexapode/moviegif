25b340c185bb873644b259c6023ebf52b63d673a
adrien <risseraka@gmail.com>
2014-03-15 11:40:30 +0000

    scaffolding express+bootstrap with yeoman

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'moviegif'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/moviegif-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'moviegif'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/moviegif-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'moviegif'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/moviegif-production'
+  }
+};
+
+module.exports = config[env];
