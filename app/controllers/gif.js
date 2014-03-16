d22c6457c4c61b273e1fe9b8768fd05e2930dd7e
Pierre-Loic Doulcet <pierre.doulcet@webdoc.com>
2014-03-16 11:39:33 +0100

    Add next / pref in single post view

--- app/controllers/gif.js
+++ app/controllers/gif.js
@@ -14,9 +14,22 @@ exports.index = function(req, res) {
     }, function(err, results){
 	if (err) throw new Error(err);
 
+  var arr = id.split('_');
+  var num = parseInt(arr[arr.length - 1].replace('.gif', ''));
+  var name = '';
+  for (i = 0; i < arr.length - 1; ++i) {
+    if (i > 0) {
+      name += '_';
+    }
+    name += arr[i];
+  }
+  name += '_';
+
 	res.render('gif/index', {
 	    title: 'Gif: ' + id,
-	    gif: id
+	    gif: id,
+      previous : name + (num - 1) + '.gif',
+      next :  name + (num + 1) + '.gif'
 	});
     });
 };
