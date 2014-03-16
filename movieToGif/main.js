8ce2f1c347f933c88ab7b7359d2c4880f1e15e98
Adrien Risser <risseraka@gmail.com>
2014-03-16 11:57:24 +0100

    fixing other various issues

--- movieToGif/main.js
+++ movieToGif/main.js
@@ -157,17 +157,29 @@ function generateNext() {
 var SUB_GENERATED = 0;
 function generateSubtitle(target, str) {
     var size = 35;
+    var strokeSize = "1.8";
     if (str.length > 30) {
         size = 25;
+	strokeSize = "1.2";
     }
     if (str.length > 60) {
         size = 20;
+	strokeSize = "0.8";
     }
-    im.convert(['-background', 'transparent', '-font', 'Arial', '-pointsize', size, '-fill', 'white', '-size', WIDTH + 'x', '-gravity', 'Center', '-stroke', 'black', '-strokewidth', '1.8', 'caption:' + str , target], 
-	       function(err, stdout){
-		   if (err) {console.log(err);}
-		   mergeWaterMark();
-	       });
+    im.convert([
+	'-background', 'transparent',
+	'-font', 'Arial',
+	'-pointsize', size,
+	'-fill', 'white',
+	'-size', WIDTH + 'x',
+	'-gravity', 'Center',
+	'-stroke', 'black',
+	'-strokewidth', strokeSize,
+	'caption:' + str , target
+    ], function(err, stdout){
+	if (err) {console.log(err);}
+	mergeWaterMark();
+    });
 }
 
 var FUSIONED_WATERMARKS = 0;
