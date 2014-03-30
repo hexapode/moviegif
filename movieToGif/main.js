03f95eec8694d18bf6d38d820408df0165bd2dc3
Pierre-Loic Doulcet <pierre.doulcet@webdoc.com>
2014-03-30 20:55:53 +0200

    qscale => 20

--- movieToGif/main.js
+++ movieToGif/main.js
@@ -187,7 +187,7 @@ function takeAllScreenShots(callback) {
         + ' -t ' + duration
         + ' -s ' + WIDTH + 'x' + HEIGHT
         + ' -r ' + FRAME_RATE
-        + ' -qscale 1'
+        + ' -qscale 20'
         + ' ' + TEMP_DIR + 'frame_' + CURRENT + '_%d.jpg';
 
     console.log(ffmpegCommand);
