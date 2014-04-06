fad8f7db727c3183e5ec75e87063a2a2330ce33a
Pierre-Loic Doulcet <pierre.doulcet@webdoc.com>
2014-04-06 19:25:03 +0200

    Improve site

--- app/controllers/gif.js
+++ app/controllers/gif.js
@@ -8,28 +8,29 @@ exports.index = function(req, res) {
     var id = req.params.id;
 
     client.search({
-	index: 'srt',
-	type: 'srt',
-	id: id
+      index: 'srt',
+      type: 'srt',
+	    id: id
     }, function(err, results){
-	if (err) throw new Error(err);
-  console.log('RESULT' + results);
-  var arr = id.split('_');
-  var num = parseInt(arr[arr.length - 1].replace('.gif', ''));
-  var name = '';
-  for (i = 0; i < arr.length - 1; ++i) {
-    if (i > 0) {
+    	if (err) throw new Error(err);
+      console.log('RESULT' + results);
+      var arr = id.split('_');
+      var num = parseInt(arr[arr.length - 1].replace('.gif', ''));
+      var name = '';
+      for (i = 0; i < arr.length - 1; ++i) {
+        if (i > 0) {
+          name += '_';
+        }
+        name += arr[i];
+      }
       name += '_';
-    }
-    name += arr[i];
-  }
-  name += '_';
 
-	res.render('gif/index', {
-	    title: 'Gif: ' + id,
-	    gif: id,
-      previous : name + (num - 1) + '.gif',
-      next :  name + (num + 1) + '.gif'
-	});
+    	res.render('gif/index', {
+    	    title: 'Gif: ' + id,
+    	    gif: id,
+          previous : name + (num - 1) + '.gif',
+          next :  name + (num + 1) + '.gif',
+          results : JSON.stringify(results)
+    	});
     });
 };
